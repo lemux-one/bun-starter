7398b30ffbf52ddbdc955b6c60b252b9b4d1fec8
Luis Lemus <lemux.one@gmail.com>
2025-03-16 21:31:25 -0400

    Update environment file paths and adjust git configuration in initialization scripts

--- .devcontainer/scripts/initialize.sh
+++ .devcontainer/scripts/initialize.sh
@@ -1,5 +1,5 @@
-EXAMPLE_ENV="example.env"
-ENV_FILE=".env"
+EXAMPLE_ENV=".devcontainer/example.env"
+ENV_FILE=".devcontainer/.env"
 if [ ! -f "$ENV_FILE" ]; then
   cp "$EXAMPLE_ENV" "$ENV_FILE"
   echo "$ENV_FILE file created from $EXAMPLE_ENV"
